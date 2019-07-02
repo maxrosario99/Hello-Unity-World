198441a23ed4dc43d900015b1bb81c3943ab0e4b
maxrosario99 <maxumum1000@gmail.com>
2019-07-01 21:28:00 -0400

    First commit

---
+++ Hello Unity World/Assets/ChangeName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class ChangeName: MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+        TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+        textmeshPro.SetText("Hello World from Max Rosario"); 
+        }
+    }
+}
